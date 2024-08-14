3815b3223c9b20541e4b78f823bfd61c53c03f4b
Matias Lucero <matiasjavierlucero@gmail.com>
2024-08-13 23:03:27 -0300

    render_kw

--- forms.py
+++ forms.py
@@ -3,5 +3,7 @@ from wtforms.fields import StringField, IntegerField, SubmitField
 
 
 class MarcaForm(FlaskForm):
-    nombre = StringField('NOMBRE')
-    submit = SubmitField('Guardar')
+    nombre = StringField('NOMBRE', render_kw={
+        "class":"form-control", "placeholder":"Nombre"
+    })
+    submit = SubmitField('Guardar', render_kw={"class":"btn btn-success"})
