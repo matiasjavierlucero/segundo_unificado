9b2a04f5a01e4a4933767e8dcc202505cc168c2a
Matias Lucero <matiasjavierlucero@gmail.com>
2024-08-20 20:05:02 -0300

    add style

--- forms.py
+++ forms.py
@@ -3,7 +3,9 @@ from wtforms.fields import StringField, IntegerField, SubmitField
 
 
 class MarcaForm(FlaskForm):
-    nombre = StringField('NOMBRE', render_kw={
-        "class":"form-control", "placeholder":"Nombre"
+    nombre = StringField('NOMBRE',
+        render_kw={
+        "class":"form-control",
+        "placeholder":"Nombre"
     })
     submit = SubmitField('Guardar', render_kw={"class":"btn btn-success"})
